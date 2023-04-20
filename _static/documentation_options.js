a055a6d991c3bad30921387cdcdcc86a5f895dbd
stefanv <sjvdwalt@gmail.com>
2023-04-20 05:26:49 +0000

    Deploying to gh-pages from  @ b9e57790b9414fd2d0e0bde54fd9ec0f1324bb63 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.2.dev17+g390e971',
+    VERSION: '0.11.2.dev21+gb9e5779',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
