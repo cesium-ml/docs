b8693ac65fa8438864d26ff024e73a437d52fb35
profjsb <profjsb@gmail.com>
2023-04-20 04:30:13 +0000

    Deploying to gh-pages from  @ 390e9711551c2293935806390ba25f7b1a8cf170 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.2.dev16+gd05adf4',
+    VERSION: '0.11.2.dev17+g390e971',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
